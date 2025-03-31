ff9ad8f27c31797d7f88f92c30b69476afe46c84
Lukas Talaga <ltalaga1@gmail.com>
2025-03-31 18:34:30 +0000

    fixed db_test and created db_create

--- app.py
+++ app.py
@@ -20,9 +20,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Lukas Talaga in 3308'
 
-# Testing PostgreSQL database connection success
+# Tests and verifies PostgreSQL database connection success
 @app.route('db_test')
 def testing():
-    conn = psycopg2.("https://lukas-talaga-cspb-3308-lab-10.onrender.com") # connects to the PostgreSQL database
+    conn = psycopg2.connect("postgresql://lukas_talaga_cspb_3308_lab_10_postgresql_user:EVJVGUJi7oBvBRLjnN7km0zZmVBzwrDv@dpg-cvldj32dbo4c73dai8d0-a/lukas_talaga_cspb_3308_lab_10_postgresql") # connects to the PostgreSQL database
     conn.close() # closes the connection
     return "Database Connection Successful" # Displayed on the browser to show confirmation that this function ran and connected to the db
+
+# Creates and returns verification of an example table with Basketball information
+@app.route('db_create')
+def creating():
+    conn = psycopg2.connect('postgresql://lukas_talaga_cspb_3308_lab_10_postgresql_user:EVJVGUJi7oBvBRLjnN7km0zZmVBzwrDv@dpg-cvldj32dbo4c73dai8d0-a/lukas_talaga_cspb_3308_lab_10_postgresql')
+    cur = conn.cursor() # create a cursor to interact with database data
+    # uses SQL to create sample table
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit() # Makes the database changes remain after closing connection
+    conn.close() # Close the database
+    return "Basketball Table Successfully Created" # Display verification in browser that function ran
