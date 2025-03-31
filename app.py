87d0f72ffd1df665496ceefef545fc5043716e2d
Lukas Talaga <ltalaga1@gmail.com>
2025-03-31 21:45:08 +0000

    completed db_select and db_drop

--- app.py
+++ app.py
@@ -63,3 +63,37 @@ def insert_into_db():
     conn.commit() # commits changes to the db
     conn.close() # close the db connection
     return "Basketball Table Successfully Populated" # return verification that the function ran
+
+# Displays all the data currently in the database table "Basketball"
+@app.route('/db_select')
+def select_from_db():
+    conn = psycopg2.connect('postgresql://lukas_talaga_cspb_3308_lab_10_postgresql_user:EVJVGUJi7oBvBRLjnN7km0zZmVBzwrDv@dpg-cvldj32dbo4c73dai8d0-a/lukas_talaga_cspb_3308_lab_10_postgresql') # connects to the PostgreSQL database
+    cur = conn.cursor() # create a cursor to interact with database data
+    # uses SQL to create sample table
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    cursor_response = cur.fetchall()
+    conn.close() # close the db connection
+    table_data = ""
+    table_data += "<table>" # add table HTML tag before data to be able to display on browser
+    for row in cursor_response:
+        table_data += "<tr>" # prepend each row in db with table row tag
+        for attribute in row:
+            table_data += "<td>{}</td>".format(attribute)
+        table_data += "</tr>" # append each row in db with table row tag
+    table_data += "</table>" # close the table tag
+    return table_data # display the table data
+
+# Drops the Basketball table from the PostgreSQL database
+@app.route('/db_drop')
+def drop_from_db():
+    conn = psycopg2.connect('postgresql://lukas_talaga_cspb_3308_lab_10_postgresql_user:EVJVGUJi7oBvBRLjnN7km0zZmVBzwrDv@dpg-cvldj32dbo4c73dai8d0-a/lukas_talaga_cspb_3308_lab_10_postgresql') # connects to the PostgreSQL database
+    cur = conn.cursor() # create a cursor to interact with database data
+    # uses SQL to create sample table
+    cur.execute('''
+        DROP TABLE Basketball;
+    ''')
+    conn.commit() # commits changes to the db
+    conn.close() # close the db connection
+    return "Basketball Table Successfully Dropped" # return verification that the function ran
