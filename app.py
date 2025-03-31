864b7bebaaa3d989d46ca7b3ebe2c29f91fc6373
Lukas Talaga <ltalaga1@gmail.com>
2025-03-31 18:18:30 +0000

    created db_test

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
+#
+#
+#
+# Author: Lukas Talaga
+# CUID: Luta2112
+# GitHub: Lukas-Talaga
+# 
+# Purpose: This is the file used by Render to host a basic flask app web service.
+#          The application uses PostgreSQL to store, manipulate, and retrieve data displayed by the browser.
+#
+#
+#
+
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Lukas Talaga in 3308'
+
+# Testing PostgreSQL database connection success
+@app.route('db_test')
+def testing():
+    conn = psycopg2.("https://lukas-talaga-cspb-3308-lab-10.onrender.com") # connects to the PostgreSQL database
+    conn.close() # closes the connection
+    return "Database Connection Successful" # Displayed on the browser to show confirmation that this function ran and connected to the db
